b28c87bf7a3bfc32f55676b4ff8ca0a90a8bcd3d
Lambda931 <robert.meek1993@gmail.com>
2025-12-10 01:26:04 +0000

    Cleaned up Header code

--- Windows/Main Window/MainWindow.HeaderManager.cs
+++ Windows/Main Window/MainWindow.HeaderManager.cs
@@ -1,5 +1,6 @@
 using Dalamud.Bindings.ImGui;
 using Dalamud.Interface.Textures.TextureWraps;
+using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 
@@ -10,207 +11,89 @@ public partial class MainWindow
     private IDalamudTextureWrap headerBackground;// = Icons.ConvertToTextureWrap(BetterTeleport.JournalSeparatorTexture);
     private Vector4 headerTextColour = new Vector4(1f, 0.85f, 0f, 1f);
 
-
     private void DrawTopHeader()
     {
-        switch (currentTab)
+        var header = currentTab switch
         {
-            case Tab.All:
-                DrawTableSectionHeader("Residential Areas", headerBackground, headerTextColour);
-                break;
-            case Tab.Residential:
-                DrawTableSectionHeader("Residential Areas", headerBackground, headerTextColour);
-                break;
-            case Tab.LaNoscea:
-                DrawTableSectionHeader("La Noscea", headerBackground, headerTextColour);
-                break;
-            case Tab.BlackShroud:
-                DrawTableSectionHeader("The Black Shroud", headerBackground, headerTextColour);
-                break;
-            case Tab.Thanalan:
-                DrawTableSectionHeader("Thanalan", headerBackground, headerTextColour);
-                break;
-            case Tab.Ishgard:
-                DrawTableSectionHeader("Coerthas", headerBackground, headerTextColour);
-                break;
-            case Tab.GyrAbania:
-                DrawTableSectionHeader("Gyr Abania", headerBackground, headerTextColour);
-                break;
-            case Tab.FarEast:
-                DrawTableSectionHeader("Hingashi", headerBackground, headerTextColour);
-                break;
-            case Tab.IndependentNations:
-                DrawTableSectionHeader("Mor Dhona", headerBackground, headerTextColour);
-                break;
-            case Tab.Ilsabard:
-                DrawTableSectionHeader("Ilsabard", headerBackground, headerTextColour);
-                break;
-            case Tab.Tural:
-                DrawTableSectionHeader("Yok Tural", headerBackground, headerTextColour);
-                break;
-            case Tab.Norvrandt:
-                DrawTableSectionHeader("Norvrandt", headerBackground, headerTextColour);
-                break;
-            case Tab.BeyondTheSource:
-                DrawTableSectionHeader("The Sea of Stars", headerBackground, headerTextColour);
-                break;
-            case Tab.MarketBoards:
-                DrawTableSectionHeader("Market Boards", headerBackground, headerTextColour);
-                break;
-            case Tab.SummoningBells:
-                DrawTableSectionHeader("Summoning Bells", headerBackground, headerTextColour);
-                break;
-            case Tab.AlliedSocieties:
-                DrawTableSectionHeader("A Realm Reborn", headerBackground, headerTextColour);
-                break;
-            case Tab.CustomDeliveries:
-                DrawTableSectionHeader("Custom Deliveries", headerBackground, headerTextColour);
-                break;
-            case Tab.DeepDungeons:
-                DrawTableSectionHeader("Deep Dungeons", headerBackground, headerTextColour);
-                break;
-            case Tab.RestorationContent:
-                DrawTableSectionHeader("Restoration Content", headerBackground, headerTextColour);
-                break;
-            case Tab.FieldOperations:
-                DrawTableSectionHeader("Field Operations", headerBackground, headerTextColour);
-                break;
-        }
+            Tab.All => "Residential Areas",
+            Tab.Residential => "Residential Areas",
+            Tab.LaNoscea => "La Noscea",
+            Tab.BlackShroud => "The Black Shroud",
+            Tab.Thanalan => "Thanalan",
+            Tab.Ishgard => "Coerthas",
+            Tab.GyrAbania => "Gyr Abania",
+            Tab.FarEast => "Hingashi",
+            Tab.IndependentNations => "Mor Dhona",
+            Tab.Ilsabard => "Ilsabard",
+            Tab.Tural => "Yok Tural",
+            Tab.Norvrandt => "Norvrandt",
+            Tab.BeyondTheSource => "The Sea of Stars",
+            Tab.MarketBoards => "Market Boards",
+            Tab.SummoningBells => "Summoning Bells",
+            Tab.AlliedSocieties => "A Realm Reborn",
+            Tab.CustomDeliveries => "Custom Deliveries",
+            Tab.DeepDungeons => "Deep Dungeons",
+            Tab.RestorationContent => "Restoration Content",
+            Tab.FieldOperations => "Field Operations",
+            _ => "Unknown"
+        };
+
+        DrawTableSectionHeader(header, headerBackground, headerTextColour);
     }
+
+    private static readonly Dictionary<Tab, Dictionary<uint, string>> HeaderMap = new()
+    {
+        [Tab.All] = new()
+        {
+            { 55, "The Black Shroud" }, { 7, "Thanalan" }, { 62, "Coerthas" }, { 71, "Abalathia's Spine" },
+            { 74, "Dravania" }, { 79, "Gyr Abania" }, { 103, "Hingashi" }, { 111, "Othard" }, { 212, "Mor Dhona" },
+            { 24, "The Northern Empty" }, { 127, "Ilsabard" }, { 173, "Yok Tural" }, { 206, "Xak Tural" },
+            { 168, "Norvrandt" }, { 148, "The Sea of Stars" }, { 181, "The World Unsundered" }, { 178, "Unlost World" },
+        },
+
+        [Tab.Ishgard] = new()
+        {
+            { 71, "Abalathia's Spine" }, { 74, "Dravania" }
+        },
+
+        [Tab.FarEast] = new()
+        {
+            { 111, "Othard" }
+        },
+
+        [Tab.IndependentNations] = new()
+        {
+            { 24, "The Northern Empty" }
+        },
+
+        [Tab.Tural] = new()
+        {
+            { 206, "Xak Tural" }
+        },
+
+        [Tab.BeyondTheSource] = new()
+        {
+            { 181, "The World Unsundered" }, { 178, "Unlost World" }
+        },
+
+        [Tab.AlliedSocieties] = new()
+        {
+            { 7, "Heavensward" }, { 79, "Stormblood" }, { 128, "Shadowbringers" }, { 136, "Endwalker" }, { 181, "Dawntrail" }
+        }
+    };
 
     private void DrawHeaderInNextRow(uint id)
     {
         LocationManager.GetCurrentResidentialIDs();
         var lastResidentialID = LocationManager.currentResidentialIDs.Last();
-        if(currentTab == Tab.All && id == lastResidentialID)
-        {
-            DrawTableSectionHeader("La Noscea", headerBackground, headerTextColour);
-        }
 
-        switch (currentTab)
+        if (currentTab == Tab.All && id == lastResidentialID)
+            DrawTableSectionHeader("La Noscea", headerBackground, headerTextColour);
+
+        if (HeaderMap.TryGetValue(currentTab, out var idMap) &&
+            idMap.TryGetValue(id, out var header))
         {
-            case Tab.All:
-                switch (id)
-                {
-                    case 55:
-                        DrawTableSectionHeader("The Black Shroud", headerBackground, headerTextColour);
-                        break;
-                    case 7:
-                        DrawTableSectionHeader("Thanalan", headerBackground, headerTextColour);
-                        break;
-                    case 62:
-                        DrawTableSectionHeader("Coerthas", headerBackground, headerTextColour);
-                        break;
-                    case 71:
-                        DrawTableSectionHeader("Abalathia's Spine", headerBackground, headerTextColour);
-                        break;
-                    case 74:
-                        DrawTableSectionHeader("Dravania", headerBackground, headerTextColour);
-                        break;
-                    case 79:
-                        DrawTableSectionHeader("Gyr Abania", headerBackground, headerTextColour);
-                        break;
-                    case 103:
-                        DrawTableSectionHeader("Hingashi", headerBackground, headerTextColour);
-                        break;
-                    case 111:
-                        DrawTableSectionHeader("Othard", headerBackground, headerTextColour);
-                        break;
-                    case 212:
-                        DrawTableSectionHeader("Mor Dhona", headerBackground, headerTextColour);
-                        break;
-                    case 24:
-                        DrawTableSectionHeader("The Northern Empty", headerBackground, headerTextColour);
-                        break;
-                    case 127:
-                        DrawTableSectionHeader("Ilsabard", headerBackground, headerTextColour);
-                        break;
-                    case 173:
-                        DrawTableSectionHeader("Yok Tural", headerBackground, headerTextColour);
-                        break;
-                    case 206:
-                        DrawTableSectionHeader("Xak Tural", headerBackground, headerTextColour);
-                        break;
-                    case 168:
-                        DrawTableSectionHeader("Norvrandt", headerBackground, headerTextColour);
-                        break;
-                    case 148:
-                        DrawTableSectionHeader("The Sea of Stars", headerBackground, headerTextColour);
-                        break;
-                    case 181:
-                        DrawTableSectionHeader("The World Unsundered", headerBackground, headerTextColour);
-                        break;
-                    case 178:
-                        DrawTableSectionHeader("Unlost World", headerBackground, headerTextColour);
-                        break;
-                }
-                break;
-            case Tab.Ishgard:
-                switch (id)
-                {
-                    case 71:
-                        DrawTableSectionHeader("Abalathia's Spine", headerBackground, headerTextColour);
-                        break;
-                    case 74:
-                        DrawTableSectionHeader("Dravania", headerBackground, headerTextColour);
-                        break;
-                }
-                break;
-            case Tab.FarEast:
-                switch (id)
-                {
-                    case 111:
-                        DrawTableSectionHeader("Othard", headerBackground, headerTextColour);
-                        break;
-                }
-                break;
-            case Tab.IndependentNations:
-                switch (id)
-                {
-                    case 24:
-                        DrawTableSectionHeader("The Northern Empty", headerBackground, headerTextColour);
-                        break;
-                }
-                break;
-            case Tab.Tural:
-                switch (id)
-                {
-                    case 206:
-                        DrawTableSectionHeader("Xak Tural", headerBackground, headerTextColour);
-                        break;
-                }
-                break;
-            case Tab.BeyondTheSource:
-                switch (id)
-                {
-                    case 181:
-                        DrawTableSectionHeader("The World Unsundered", headerBackground, headerTextColour);
-                        break;
-                    case 178:
-                        DrawTableSectionHeader("Unlost World", headerBackground, headerTextColour);
-                        break;
-                }
-                break;
-            case Tab.AlliedSocieties:
-                switch (id)
-                {
-                    case 7:
-                        DrawTableSectionHeader("Heavensward", headerBackground, headerTextColour);
-                        break;
-                    case 79:
-                        DrawTableSectionHeader("Stormblood", headerBackground, headerTextColour);
-                        break;
-                    case 128:
-                        DrawTableSectionHeader("Shadowbringers", headerBackground, headerTextColour);
-                        break;
-                    case 136:
-                        DrawTableSectionHeader("Endwalker", headerBackground, headerTextColour);
-                        break;
-                    case 181:
-                        DrawTableSectionHeader("Dawntrail", headerBackground, headerTextColour );
-                        break;
-                }
-                break;
+            DrawTableSectionHeader(header, headerBackground, headerTextColour);
         }
     }
 
@@ -260,4 +143,3 @@ public partial class MainWindow
         ImGui.Dummy(new Vector2(fullWidth, totalHeight + 2));
     }
 }
-        
